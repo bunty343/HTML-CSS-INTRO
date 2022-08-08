afd7bc0a16715c3846c1e7943c4f0f8c57a3109b
bunty343 <107154389+bunty343@users.noreply.github.com>
2022-08-08 12:51:00 -0700

    hi

---
+++ javascript/intro.js
@@ -0,0 +1,27 @@
+// variable declaration
+// var myVar = "Tai" // declared a variable, but have not assigned a value
+// console.log("myVar", myVar) // var is not defined - undefined (valid data type/ value)
+// hoisted
+
+// ourName = "Tai" // "Tai" string -> assigned a value of "Tai"
+// console.log("ourName", ourName)
+
+// other data types - 5 main primitive data type
+// undefined, null, boolean, string, number
+// myVar = null
+// console.log("type", typeof myVar)
+// JS goes through 2 phases: 1) memory allocation phase 2) execution phase
+
+// console.log(myVar = 10) -> = is the assignment operator
+// == or === is the comparison operator
+
+let myVar
+console.log(23%5)
+
+// write function to determine odd or even
+
+
+
+
+
+

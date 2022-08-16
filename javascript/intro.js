103eac7529adf55c154afe56d62620f9fa2e727a
bunty343 <107154389+bunty343@users.noreply.github.com>
2022-08-16 12:13:35 -0700

    hmm

--- javascript/intro.js
+++ javascript/intro.js
@@ -1,147 +1,35 @@
-// variable declaration
-// var myVar = "Bunty" // declared a variable, but have not assigned a value
-// console.log("myVar", myVar) // var is not defined - undefined (valid data type/ value)
-// hoisted
 
-// ourName = "Bunty" // "Bunty" string -> assigned a value of "Bunty"
-// console.log("ourName", ourName)
+// // var arr =  [32, 47, 1, 3, 5, 50, 9, 10] ;
+// // var highestNum = arr[2];
 
-// other data types - 5 main primitive data type
-// undefined, null, boolean, string, number
-// myVar = null
-// console.log("type", typeof myVar)
-// JS goes through 2 phases: 1) memory allocation phase 2) execution phase
+// // for (var i = 2; i < arr.length; i++) {
+// //     if (highestNum < arr[i] ) {
+// //         highestNum = arr[i];
+// //     }
+// //  }
+// // console.log(highestNum);
 
-// console.log(myVar = 10) -> = is the assignment operator
-// == or === is the comparison operator
+// var arr = [14, 58, 20, 77, 66, 82, 42, 67, 42, 4]
+// var max = Math.max.apply(Math, arr)
+// console.log(max)
+// Iterative java program to reverse an
+// array
 
-// let myVar
-// console.log(23%5) // modeulo ( in this moudelo is 3)
-
-// write function to determine odd or even
-
-// let myVar = 3
-
-// function isOdd(number) {   
-//     // const myVar = 10  
-// const remainder = number % 2
-// console.log(myVar)
-// if ( remainder === 1 ){
-//     return true
-// } else {
-//     return false
-// }
-// }
-// console.log(isOdd(55))
-
-// console.log ('my function is done')
-
-// javascript object => collection of key, key value pairs that are related to particular objects.
-// const dog = {
-// numOfLegs: 4,
-// numOfEyes : 2,
-// name : "Sparky"
-// isAlive: true
-// //
-// bark: function () {
-//     console.log ('ruff ruff')
-// }
-// }
-// console.log(dog) // object oriented programming
-
-// const myStr = "sccsjcdcccc"
-// console.log(myStr.length)
-
-// javascript arrays []
-// diff bw arrays and objects
-// 1 arrays are not key values pairs
-// 2. inside of array , the order of items matter - unside object, the order of key / value pairs donot matter
-// elements (items) inside of arrays do not have to be unique - inside objects, keys have to unique
-// const myEmptyArr = []
-// const myArr = ['item1',"item1",1, true,null,undefined,dog, myEmptyArr, {},[]]
-
-
-// console.log(myArr)
-// Write a function called “add” that takes in 2 numbers and returns their added value.
-var sum = add(12, 13);
-
-function add (a, b) {
-  return a + b;
-}
-console.log(sum);
-//Write a function called “divide” that takes in 2 numbers and returns the divided value.
-
-var sum = divide(12, 13);
-
-function sum(a, b) {
-  return a * b;
-}
-console.log(sum);
-// Write a function called “convert” that takes in a fahrenheit value and converts it into celsius. The formula for conversion is :  Celsius = (Fahrenheit  - 32) * 5/9
-
-function toCelsius(f) {
-    return (5/9) * (f-32);
-  }
-// Write a function called “divisibleBy55” that takes in a number and will return either true or false to let us know whether the number is divisible by 55 or not. If a number is divisible by 55 that means that there are 0 remainders.
-var divide = myFunction(44, 33);
-function myFunction(a, b) {
-  return a / b;
-}
-console.log(divide)
-let myVar = 10
-function divisibleBy55(number) {   
-const remainder = number % 2
-console.log(myVar)
-if ( remainder === 0 ){
-    return true
-} else {
-    return false
-}
-}
-console.log(divisibleBy55(55))
-
-// Write a function called “checkLength” that takes in a string and return a number indicating the string’s length.
-
- const checkLength = "Waterfall"
- console.log(checkLength.length)
-
- // Write a function called “concat” that takes in 2 strings and returns the concatenated string. Ex: “cat” and “dog” will return “cat dog”.
-
-
-const conCat1 = "Hindu";
-const conCat2 = "Stan";
-const concat = ""  + conCat1 + conCat2
-console.log(concat); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//function to reverse a string in JavaScript 
+//str is the string you want reverse  
+//convert the string into array of characters,reverse the array using 
+//reverse() function and join back the array characters 
+ 
+// function reverseString(){  
+//     return str.split("").reverse().join("");    
+//  }
+function reverse("hello") { 
+	if ("hello".length === 0) return '' 
+	return reverse("hello".slice(1)) + t"hello"[0]  
+} 
+console.log(reverse)
+    
+    
 
 
 
